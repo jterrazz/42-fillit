0a1c01cbcad380b9523af9e3667ab9cabc5d5768
Jean-baptiste TERRAZZONI <jterrazz@e1r2p8.42.fr>
2017-04-17 22:59:58 +0200

    ca avance

--- src/resolver.c
+++ src/resolver.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 17:45:34 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/17 19:38:13 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/17 19:50:47 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ t_map		 	*ft_create_map(char *input)
 
 	(void)input;
 
-	map = (t_map *)malloc(sizeof(t_map)); // A verifier par le grand mickael
-	if (map == NULL)
+	if(!(map = (t_map *)malloc(sizeof(t_map)))) // A verifier par le grand mickael
 		return (NULL);
 	map->size = 8;
 	map->map = ft_strnew(8 * 8);
@@ -29,43 +28,43 @@ t_map		 	*ft_create_map(char *input)
 	return (map);
 }
 
-//int				ft_put_piece(t_map *map, char *input, int nb_piece)
+//int				ft_place(t_map *map, char *input, int nb_piece)
 //{
 //	if (nb_piece == 3)
 //		return (0);
 //	return (1);
 //}
 
-//int				ft_clean_piece(t_map *map, int nb_piece)
+//int				ft_clean(t_map *map, int nb_piece)
 //{
 //	return (0);
 //}
 
-//void			ft_put_pieces(t_map *map, char *input, int nb_piece)
-//{
-//	int i;
-//	int size_sq;
-//
-//	i = 0;
-//	size_sq = map->size * map->size;
-//	while (i < size_sq)
-//	{
-//		if (ft_put_piece(map, input, nb_piece)) // Renvois 0 si pas possible ou si derniere piece
-//			ft_put_pieces(map, input, nb_piece + 1);
+void			ft_put_pieces(t_map *map, t_map *map_f, char *input, int nb_piece)
+{
+	int i;
+	int size_sq;
+
+	i = 0;
+	size_sq = map->size * map->size;
+	while (i < size_sq)
+	{
+//		if (ft_place(map, map_f, input, nb_piece)) // Renvois 0 si pas possible ou si derniere piece
+//			ft_put_pieces(map, map_f input, nb_piece + 1);
 //		else
-//			ft_clean_piece(map, nb_piece);
-//		i++;
-//	}
-//}
+//			ft_clean(map, nb_piece);
+		i++;
+	}
+}
 
 int				ft_resolver(char *input)
 {
 	t_map	*map;
+	t_map	*map_f;
 
 	map = ft_create_map(input);
-	ft_print_map(map->map, map->size);
-	if (map == NULL)
+	if(!(map_final = ft_create_map(input)))
 		return (0);
-//	ft_put_pieces(map, input, 0);
+	ft_put_pieces(map, map_f input, 0);
 	return (1);
 }
