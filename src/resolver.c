517359b1f6a86c04cf0aa15b76b9bc2265d326cf
Jean-baptiste TERRAZZONI <jterrazz@e1r3p9.42.fr>
2017-04-18 17:36:14 +0200

    erg

--- src/resolver.c
+++ src/resolver.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 17:45:34 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/18 15:52:29 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/18 17:35:50 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,35 +25,6 @@ t_map		 	*ft_create_map(int n)
 		return (NULL);
 	return (map);
 }
-
-//int				ft_place(t_map *map, char *input, int nb_piece)
-//{
-//	if (nb_piece == 3)
-//		return (0);
-//	return (1);
-//}
-
-//int				ft_clean(t_map *map, int nb_piece)
-//{
-//	return (0);
-//}
-
-//void			ft_put_pieces(t_map *map, t_map *map_f, t_pieces *pieces, int nb_piece)
-//{
-//	int		i;
-//	int		size_sq;
-//
-//	i = 0;
-//	size_sq = map->size * map->size;
-//	while (i < size_sq)
-//	{
-//		if (ft_place(map, map_f, input, nb_piece)) // Renvois 0 si pas possible ou si derniere piece
-//			ft_put_pieces(map, map_f input, nb_piece + 1);
-//		else
-//			ft_clean(map, nb_piece);
-//		i++;
-//	}
-//}
 
 int				ft_set_pieces(t_pieces *pieces, char *input) // check is secure
 {
@@ -75,6 +46,7 @@ int				ft_set_pieces(t_pieces *pieces, char *input) // check is secure
 			{
 				pieces->pieces[i][i_case].x = (j - i * 21) % 5;
 				pieces->pieces[i][i_case].y = (j - i * 21) / 5;
+				pieces->pieces[i][i_case].letter = i + 'A';
 				i_case++;
 			}
 			j++;
@@ -82,6 +54,33 @@ int				ft_set_pieces(t_pieces *pieces, char *input) // check is secure
 		i++;
 	}
 	return (1);
+}
+
+void		ft_optimize_pieces(t_pieces *pieces)
+{
+	int i;
+
+	i = 0;
+	while (i < pieces->nb_of_pieces)
+	{
+		while (pieces->pieces[i][0].x && pieces->pieces[i][1].x
+				&& pieces->pieces[i][2].x && pieces->pieces[i][3].x)
+		{
+			pieces->pieces[i][0].x--;
+			pieces->pieces[i][1].x--;
+			pieces->pieces[i][2].x--;
+			pieces->pieces[i][3].x--;
+		}
+		while (pieces->pieces[i][0].y && pieces->pieces[i][1].y
+				&& pieces->pieces[i][2].y && pieces->pieces[i][3].y)
+		{
+			pieces->pieces[i][0].y--;
+			pieces->pieces[i][1].y--;
+			pieces->pieces[i][2].y--;
+			pieces->pieces[i][3].y--;
+		}
+		i++;
+	}
 }
 
 t_pieces		*ft_get_pieces(char *input)
@@ -105,32 +104,85 @@ t_pieces		*ft_get_pieces(char *input)
 	if (pieces->pieces == NULL)
 		return (0);
 	ft_set_pieces(pieces, input);
-
-
-
+	ft_optimize_pieces(pieces);
 
 	printf("Il y a %i pieces\n", pieces->nb_of_pieces); //DELLLLLLLLLLLLLL
+	printf("%i, %i\n", pieces->pieces[0][0].x, pieces->pieces[0][0].y);
+	printf("%i, %i\n", pieces->pieces[0][1].x, pieces->pieces[0][1].y);
+	printf("%i, %i\n", pieces->pieces[0][2].x, pieces->pieces[0][2].y);
+	printf("%i, %i\n", pieces->pieces[0][3].x, pieces->pieces[0][3].y);
+
 	printf("%i, %i\n", pieces->pieces[1][0].x, pieces->pieces[1][0].y);
 	printf("%i, %i\n", pieces->pieces[1][1].x, pieces->pieces[1][1].y);
 	printf("%i, %i\n", pieces->pieces[1][2].x, pieces->pieces[1][2].y);
 	printf("%i, %i\n", pieces->pieces[1][3].x, pieces->pieces[1][3].y);
 
-
-
 	return (pieces);
+}
+
+int				ft_place(t_map *map, t_case *piece, int i_map)
+{
+	x_map;
+	y_map;
+	int i;
+	int x;
+	int y;
+
+	i = 0;
+	x_map = i_map % map.size;
+	y_map = i_map / map.size;
+	while (i < 4)
+	{
+		x = x_map + piece[i].x;
+		y = y_map + piece[i].y;
+		if (map[x + y * map.size] != '.' && x < map.size - 1 && y < map.size)
+			map[x + y * map.size] = piece[0].letter;
+		else
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int				ft_clean(t_map *map, int nb_piece)
+{
+
+	return (0);
+}
+
+void			ft_put_pieces(t_map *map, t_pieces *pieces, int nb_piece, int *sol_found)
+{
+	int		i_map;
+	int		size_sq;
+
+	if (nb_pieces == pieces->nb_of_pieces)
+		sol_found = 1;
+	if (sol_found)
+		return ;
+	i_map = 0;
+	size_sq = map.size * map.size;
+	while (i_map < size_sq && sol_found)
+	{
+		if (ft_place(map, pieces[nb_pieces], i_map)) // Renvois 0 si pas possible
+			ft_put_pieces(map, pieces, nb_piece + 1, sol_found);
+		else
+			ft_clean(map, pieces[nb_pieces], i_map);
+		i++;
+	}
 }
 
 int				ft_resolver(char *input)
 {
 	t_map		*map;
-	t_map		*map_f;
 	t_pieces	*pieces;
-	
+	int			sol_found;
+
+	sol_found = 0;
 	if (!(pieces = ft_get_pieces(input)))
 		return (0); // check if it handles null
 	// Qgrqndir la map si pas de solution (partie du plus petit);
-	if (!(map_f = ft_create_map(8)) || !(map = ft_create_map(8)))
+	if (!(map = ft_create_map(8)))
 		return (0);
-//	ft_put_pieces(map, map_f, pieces, 0);
+	ft_put_pieces(map, pieces, 0, &sol_found);
 	return (1);
 }
